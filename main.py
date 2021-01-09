ad0be804102f3f3c2fdb4e180c28c84da8656740
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-01-08 23:43:44 -0500

    use list comprehension to reduce code

--- main.py
+++ main.py
@@ -83,10 +83,7 @@ while len(guessed_states) < 50:
                                                                                              "give up").title()
     # We have to check if the answer is one of the state from 50 states csv
     if answer_state == "Exit" or answer_state == "exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
